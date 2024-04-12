d550a174687151b492bbfa039b8e7e447e0736fc
Kunal-Rana-8585 <102336150+Kunal-Rana-8585@users.noreply.github.com>
2024-04-12 18:35:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const corsConfig={
   methods:["GET","POST","PUT","DELETE"],
 };
 app.options("",cors(corsConfig));
-app.use(cors{corsConfig});
+app.use(cors(corsConfig));
 
 
 app.set('views', path.join(__dirname, 'views'));
